e3b8419bef32a4def15a45f70b5717474ad4010b
only-recent <only007895@gmail.com>
2025-11-19 22:13:56 +0530

    Added remaining code

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,30 @@
 import express from "express"
 import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
+// app config
 const app = express()
 const port = 8000
 
+//middleware
 app.use(express.json())
 app.use(cors())
 
+//DB connection
+connectDB();
+
+
+//api endpoints
+app.use("/api/food",foodRouter)
+app.use("/image",express.static('uploads'))
+
 app.get("/",(req,res)=>{
-  res.send("api eorking")
+  res.send("api working")
 })
 
 app.listen(port,()=>{
   console.log(`Server started on http://localhost:${port}`)
 })
+
+
