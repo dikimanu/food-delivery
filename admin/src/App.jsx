0039aa1cf407e4f451626eb62bece1bbd29e160d
dikimanu <sp20060924@gmail.com>
2025-11-24 21:17:40 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-    const url = "http://localhost:8000"
+    const url = "https://food-delivery-backend-fl5o.onrender.com"
 
 
   return (
