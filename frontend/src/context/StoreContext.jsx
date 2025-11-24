4487978a5c90348250a44efd27c5b7ea8ae71ec6
dikimanu <sp20060924@gmail.com>
2025-11-24 20:57:08 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:8000";
+  const url = "https://food-delivery-backend-fl5o.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
